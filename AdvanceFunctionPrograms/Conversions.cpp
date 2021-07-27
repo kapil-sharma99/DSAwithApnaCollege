b63bc132a011af7fc251cbe2367a2dbadc17af79
kapil-sharma99 <ks2822123@gmail.com>
2021-07-27 21:45:54 +0530

    AdvanceFunctionPrograms: BinaryToDecimal

---
+++ AdvanceFunctionPrograms/Conversions.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int binaryToDecimal(int n)
+{
+	int ans = 0;
+	int x = 1;
+	while (n > 0)
+	{
+		int y = n % 10;
+		ans += x * y;
+		x *= 2;
+		n /= 10;
+	}
+	return ans;
+}
+
+int octalToDecimal(int n)
+{
+	int ans = 0;
+	int x = 1;
+	while (n > 0)
+	{
+		int y = n % 10;
+		ans += x * y;
+		x *= 8;
+		n /= 10;
+	}
+	return ans;
+}
+
+int32_t main()
+{
+	int n;
+	cin >> n;
+
+	cout << "BinaryToDecimal: " << binaryToDecimal(n) << endl;
+	cout << "OctalToDecimal: " << octalToDecimal(n) << endl;
+
+	return 0;
+}
