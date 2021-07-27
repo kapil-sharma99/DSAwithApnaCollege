a429b81eeadd6ba2679003ce624400155b5208df
kapil-sharma99 <ks2822123@gmail.com>
2021-07-27 21:46:27 +0530

    AdvanceFunctionPrograms: OctalToDecimal

--- AdvanceFunctionPrograms/Conversions.cpp
+++ AdvanceFunctionPrograms/Conversions.cpp
@@ -18,12 +18,12 @@ int binaryToDecimal(int n)
 int octalToDecimal(int n)
 {
 	int ans = 0;
-	int x = 1;
+	int a = 1;
 	while (n > 0)
 	{
 		int y = n % 10;
-		ans += x * y;
-		x *= 8;
+		ans += a * y;
+		a *= 8;
 		n /= 10;
 	}
 	return ans;
