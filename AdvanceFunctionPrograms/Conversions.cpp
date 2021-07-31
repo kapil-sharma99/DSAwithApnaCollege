ad892183a3d12b53ea2d96ea795d44837894e082
kapil-sharma99 <ks2822123@gmail.com>
2021-07-31 13:58:36 +0530

    AdvanceFunctionPrograms: Conversions completed

--- AdvanceFunctionPrograms/Conversions.cpp
+++ AdvanceFunctionPrograms/Conversions.cpp
@@ -37,7 +37,85 @@ int hexadecimalToDecimal(string n)
 	int s = n.size();
 	for (int i = s - 1; i >= 0; i--)
 	{
+		if (n[i] >= '0' && n[i] <= '9')
+		{
+			ans += x * (n[i] - '0');
+		}
+		else if (n[i] >= 'A' && n[i] <= 'F')
+		{
+			ans += x * (n[i] - 'A' + 10);
+		}
+		x *= 16;
 	}
+	return ans;
+}
+
+int decimalToBinary(int n)
+{
+	int x = 1;
+	int ans = 0;
+	while (x <= n)
+	{
+		x *= 2;
+	}
+	x /= 2;
+
+	while (x > 0)
+	{
+		int lastDigit = n / x;
+		n -= lastDigit * x;
+		x /= 2;
+		ans = ans * 10 + lastDigit;
+	}
+	return ans;
+}
+
+int decimalToOctal(int n)
+{
+	int x = 1;
+	int ans = 0;
+	while (x <= n)
+	{
+		x *= 8;
+	}
+	x /= 8;
+
+	while (x > 0)
+	{
+		int lastDigit = n / x;
+		n -= lastDigit * x;
+		x /= 8;
+		ans = ans * 10 + lastDigit;
+	}
+	return ans;
+}
+
+string decimalToHexaDecimal(int n)
+{
+	int x = 1;
+	string ans = "";
+	while (x <= n)
+	{
+		x *= 16;
+	}
+	x /= 16;
+
+	while (x > 0)
+	{
+		int lastDigit = n / x;
+		n -= lastDigit * x;
+		x /= 16;
+		if (lastDigit <= 9)
+		{
+			ans = ans + to_string(lastDigit);
+		}
+		else
+		{
+			char c = 'A' + lastDigit - 10;
+			ans.push_back(c);
+		}
+	}
+	return ans;
 }
 
 int32_t main()
@@ -46,10 +124,15 @@ int32_t main()
 	cin >> n;
 	string hexa;
 	cin >> hexa;
+	int deci;
+	cin >> deci;
 
 	cout << "BinaryToDecimal: " << binaryToDecimal(n) << endl;
 	cout << "OctalToDecimal: " << octalToDecimal(n) << endl;
 	cout << "HexadecimalToDecimal: " << hexadecimalToDecimal(hexa) << endl;
+	cout << "DecimalToBinary: " << decimalToBinary(deci) << endl;
+	cout << "DecimalToOctal: " << decimalToOctal(deci) << endl;
+	cout << "DecimalToHexaDecimal" << decimalToHexaDecimal(deci) << endl;
 
 	return 0;
 }
