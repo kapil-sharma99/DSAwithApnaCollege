be367dedf60fcc236bfd3ef25ac3308f7764eba8
kapil-sharma99 <ks2822123@gmail.com>
2021-08-08 14:00:34 +0530

    ArrayProgram: LinearSearch Program

---
+++ Arrays/LinearSearch.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int n, int arr[], int key)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == key)
+		{
+			return i;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	int key;
+	cin >> key;
+
+	int res = linearSearch(n, arr, key);
+	if (res == -1)
+	{
+		cout << "Element not in array!!" << endl;
+	}
+	else
+	{
+		cout << "Element found at position: " << res << endl;
+	}
+}
