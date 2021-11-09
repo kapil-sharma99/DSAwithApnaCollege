1d9401ea70932ec6d463aff58dd1e3434df856df
kapil-sharma99 <ks2822123@gmail.com>
2021-11-09 17:47:48 +0530

    Vectors: Introduction

--- Vectors/Intro.cpp
+++ Vectors/Intro.cpp
@@ -10,14 +10,22 @@ int main() {
   v.push_back(2);
   v.push_back(1);
 
+  //Method-1 for printing vectors
   for(int i = 0; i < v.size(); i++) {
     cout << v[i] << " ";
   }
   cout << endl;
 
+  //Method-2 for printing vectors
   vector<int>::iterator it;
   for(it=v.begin(); it!=v.end(); it++) {
     cout << *it << " ";
+  }
+  cout << endl;
+
+  //Method-3 for printing vectors
+  for(auto element:v) {
+    cout << element << " ";
   }
   cout << endl;
 
